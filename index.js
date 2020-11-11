43f6c6777c3f67064abfc2d88e199787ae5294d8
Marco Newman <marconewman@icloud.com>
2020-11-11 09:23:43 +0100

    set up and index

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
+
 const express = require('express');
 const app = express();
 const compression = require('compression');
+const cookieSession = require("cookie-session");
+
+app.use(express.json());
+
+app.use(cookieSession({
+    secret: "I don't have a secret",
+    maxAge: 1000 *60 *60 * 24 * 31
+}));
+
+app.use((request, response, next) => {
+    console.log("🍟", request.session);
+    next();
+});
 
 app.use(compression());
+
+const apiRoutes = require("./api-routes.js");
+app.use(apiRoutes);
 
 if (process.env.NODE_ENV != 'production') {
     app.use(
@@ -15,8 +32,27 @@ if (process.env.NODE_ENV != 'production') {
     app.use('/bundle.js', (req, res) => res.sendFile(`${__dirname}/bundle.js`));
 }
 
-app.get('*', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+
+app.get("/welcome", (request, response) => {
+
+    // If the user is logged in, they should not be here.
+    if(request.session.userId) {
+        response.redirect(302, "/");
+    } else {
+        response.sendFile(__dirname + '/index.html');
+    }
+
+});
+
+app.get('*', function(request, response) {
+
+    // If the user is NOT logged in, they should not be here.
+    if(!request.session.userId) {
+        response.redirect(302, "/welcome");
+    } else {
+        response.sendFile(__dirname + '/index.html');
+    }
+
 });
 
 app.listen(8080, function() {
